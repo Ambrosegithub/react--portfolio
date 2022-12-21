31e166bef752f95ad9f55c144b0fd609e9a38c0a
Isigba Ambrose <ambrisexyz@gmail.com>
2022-12-22 00:21:31 +0100

    added footer component

--- src/Components/Contacts/contact.jsx
+++ src/Components/Contacts/contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
         <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/> 
           <input type="email" name='email' placeholder='Your Email' required/>
-          <textarea name="message"rows="7" required></textarea>
+          <textarea name="message"rows="7" placeholder='Send Me Message' required></textarea>
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
       </div>

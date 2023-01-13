2593891003a32d6943ecd85bb4f0f6fd7f6261c5
Isigba Ambrose <ambrisexyz@gmail.com>
2023-01-13 06:43:59 +0100

    added update npm build

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -6,8 +6,8 @@ import Me from '../../assets/Full-Stack.jpg';
 
 const About = () => (
   <section id="about">
-    <h5>Get To Know </h5>
-    <h2>About Me </h2>
+    <h5>Get To Know</h5>
+    <h2>About Me</h2>
     <div className="container about__container">
       <div className="about__me">
         <div className="about__me-image">
